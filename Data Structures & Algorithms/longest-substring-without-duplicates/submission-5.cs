b9b0401e97eb30b2a9cd39ee78b38cfbc4e51f2e
Juan A. G. Portes <62453550+JuanGPortes@users.noreply.github.com>
2026-04-07 08:58:08 -0600

    Add: longest-substring-without-duplicates - submission-5

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-5.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        if(string.IsNullOrEmpty(s)){
+            return 0;
+        }
+
+        int left = 0;
+        int maxLength = 0;
+        int[] counts = new int[256]; //cold be 128 or 256 extended
+
+        for(int right = 0; right < s.Length; right++){
+            if(counts[s[right]] > 0){
+                left = Math.Max(left, counts[s[right]]);
+            }
+
+            counts[s[right]] = right + 1;
+
+            maxLength = Math.Max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+}
